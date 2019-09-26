fa257bf7ab4c81058e4e8270bf656b8ca66918d5
ejesus <disys.edilson.jesus@segurossura.com.br>
2019-09-26 08:35:42 -0300

    details

---
+++ src/components/RlistSessionContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import SessionContent from './sessionContent';
+
+export default class ListSessionContent extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                {
+                    this.props.listSessionContent.map((content, key) =>
+                        <SessionContent key={key} sessionContent={content}></SessionContent>                        
+                    )
+                }
+            </React.Fragment>
+        )
+    }
+}
