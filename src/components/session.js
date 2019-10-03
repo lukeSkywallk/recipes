1445833e71e61e4abddd5bfdb95b490bff2ec694
ejesus <disys.edilson.jesus@segurossura.com.br>
2019-10-03 08:28:12 -0300

    setting to github pages

--- src/components/session.js
+++ src/components/session.js
@@ -3,9 +3,16 @@ import ListSessionContent from './listSessionContent.js'
 import { ListItem,List, ListItemText, Collapse } from '@material-ui/core'
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
+import {makeStyles} from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    defaultStyle: {
+        marginLeft: '5%',
+    }
+});
 
 function Session({session}){
-
+    const classes = useStyles();
     const [open, setOpen] = useState(false);
 
     const handleClick = () => {
@@ -14,7 +21,7 @@ function Session({session}){
 
     return(
         <React.Fragment>
-            <List style={{marginLeft: '5%'}}>
+            <List /*style={{marginLeft: '5%'}}*/ className={classes.defaultStyle}>
                 <ListItem button onClick={handleClick}>
                     <ListItemText primary={session.nome}/>                    
                     {open ? <ExpandLess/> : <ExpandMore/>}
