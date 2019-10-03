fbc8e439c2fee07a0847d92cb3f8f1cd8c522060
ejesus <disys.edilson.jesus@segurossura.com.br>
2019-10-03 13:12:22 -0300

    Updates

---
+++ precache-manifest.1efbec95c417de53186f02e5f93c8589.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e279730565a0f23869b685fd2145aec",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "6aca7b36aab469d066f1",
+    "url": "/recipes/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0dc07f4f062d30312470",
+    "url": "/recipes/static/js/2.59ea274c.chunk.js"
+  },
+  {
+    "revision": "6aca7b36aab469d066f1",
+    "url": "/recipes/static/js/main.5ec4e0e2.chunk.js"
+  },
+  {
+    "revision": "98634b1bf32c2fc44593",
+    "url": "/recipes/static/js/runtime-main.be9e9b66.js"
+  }
+]);
